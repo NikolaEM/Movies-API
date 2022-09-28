8e2187567a1482c6a5bd10b7b4d98658c0043925
NikolaEM <stankovic.nikola2@gmail.com>
2022-09-28 11:45:13 +0200

    fixed auth

--- Movie_API/Movie_API/settings.py
+++ Movie_API/Movie_API/settings.py
@@ -83,8 +83,10 @@ REST_FRAMEWORK = {
    'DEFAULT_AUTHENTICATION_CLASSES': (
         
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
-    
+    ),  
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]  
 }
 
 WSGI_APPLICATION = 'Movie_API.wsgi.application'
--- Movie_API/users/views.py
+++ Movie_API/users/views.py
@@ -1,11 +1,11 @@
 from rest_framework.response import Response
-from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
+from rest_framework.status import HTTP_201_CREATED, HTTP_204_NO_CONTENT
 from rest_framework.mixins import RetrieveModelMixin, CreateModelMixin
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.views import APIView
-from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import IsAuthenticated
 from .models import CustomUser
-from .serializers import UserSerializer, CreateUserSerializer
+from .serializers import UserSerializer, CreateUserSerializer, BlackListTokenSerializer
 
 
 class UserViewSet(RetrieveModelMixin, GenericViewSet, CreateModelMixin):
@@ -20,7 +20,11 @@ class UserViewSet(RetrieveModelMixin, GenericViewSet, CreateModelMixin):
         return Response(UserSerializer(response, context={'request': request}).data, HTTP_201_CREATED)
 
 class BlacklistRefreshView(APIView):
+    permission_classes = [IsAuthenticated]
+
     def post(self, request):
-        token = RefreshToken(request.data.get('refresh'))
-        token.blacklist()
-        return Response("Success") 
+        serializer = BlackListTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(HTTP_204_NO_CONTENT)
+        
