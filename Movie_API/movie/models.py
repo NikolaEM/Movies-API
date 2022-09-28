2b158e60d987039e5a3bdd5606a067236ad7aa88
NikolaEM <stankovic.nikola2@gmail.com>
2022-09-28 16:46:52 +0200

    movie entity

--- Movie_API/movie/models.py
+++ Movie_API/movie/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+class Movie(models.Model):
+    title = models.CharField(max_length=40)
+    description =  models.TextField(max_length=3000)
+    movie_cover = models.FileField(upload_to='')
+    genre_choices = (
+        ('A', 'Action'),
+        ('C', 'Comedy'),
+        ('D', 'Drama'),
+        ('F', 'Fantasy'),
+        ('H', 'Horror'),
+    )
+    genre = models.CharField(max_length=1, choices=genre_choices, default='')
 
+    def __str__(self):
+        return self.title
--- Movie_API/users/urls.py
+++ Movie_API/users/urls.py
@@ -1,7 +1,5 @@
 from rest_framework.routers import SimpleRouter
 from django.urls import path
-from django.conf import settings
-from django.contrib.auth import logout
 from .views import  BlacklistRefreshView, UserViewSet
 
 usersRouter = SimpleRouter()
