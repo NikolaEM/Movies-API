1e3fe5d4b4bd46a3b273c0d98a56161466fff2bb
NikolaEM <stankovic.nikola2@gmail.com>
2022-10-07 13:30:25 +0200

    like/dislike

--- Movie_API/movie/serializers.py
+++ Movie_API/movie/serializers.py
@@ -2,11 +2,30 @@ from rest_framework import serializers
 from .models import Genre, Movie
 
 class MovieSerializer(serializers.ModelSerializer):
+    likes_count = serializers.SerializerMethodField()
+    dislikes_count = serializers.SerializerMethodField()
+
+    def get_likes_count(self, obj):
+        return obj.likes.count()
+
+    def get_dislikes_count(self, obj):
+        return obj.dislikes.count()
 
     class Meta:
         depth = 1
         model = Movie
-        fields = ('__all__')
+        fields = (
+            "likes_count",
+            "dislikes_count",
+            "title",
+            "description",
+            "movie_cover",
+            "user",
+            "genre",
+            "id"
+            )
+
+    
 
 class CreateMovieSerializer(serializers.ModelSerializer):
 
